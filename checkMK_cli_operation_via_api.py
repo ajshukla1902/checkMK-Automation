e19d1547f39f9709225d59346ec2b21a95c3ac07
ajshukla1902 <130760128+ajshukla1902@users.noreply.github.com>
2024-04-20 11:18:49 +0000

    Fixed few bugs and added a new function to handle http response.

--- checkMK_cli_operation_via_api.py
+++ checkMK_cli_operation_via_api.py
@@ -8,58 +8,61 @@
 ###########################################
 
 import argparse
-import requests 
-from operator import add
-from functools import reduce
+import requests
 
+def send_request(url, params):
+    try:
+        response = requests.post(url=url, data=params)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+        return response
+    except requests.exceptions.RequestException as e:
+        print(f"API request failed: {e}")
+        return None
 
-def Main():
-
+def main():
     url = "http://check-mk.any/central/check_mk/view.py?"
 
-    if 'add' in args.operation:
-        param = {
-                '_username' : args.username,
-                '_secret' : args.password,
-                '_transid' : '-1',
-                '_do_confirm' : 'yes',
-                '_do_actions' : 'yes',
-                'host' : args.phost,
-                '_down_from_now' : 'yes',
-                '_down_minutes' : args.time,
-                '_down_comment' : args.comment,
-                'view_name' : 'hoststatus'
-            }
+    params = {
+        '_username': args.username,
+        '_secret': args.password,
+        '_transid': '-1',
+        '_do_actions': 'yes',
+        'view_name': 'hoststatus',
+        'host': args.host,
+    }
 
-    if 'remove' in args.operation:
-        param = {
-                '_do_confirm' : 'yes',
-                '_transid' : '-1',
-                '_do_actions' : 'yes',
-                '_username' : args.username,
-                '_secret' : args.password,
-                'view_name' : 'hoststatus',
-                'host' : args.phost,
-                '_down_remove' : 'Remove'
-            }
+    if args.operation == 'add':
+        params.update({
+            '_do_confirm': 'yes',
+            '_down_from_now': 'yes',
+            '_down_minutes': args.time,
+            '_down_comment': args.comment,
+        })
+        params['_down_remove'] = ''  # Remove downtime flag for add operation
 
-    try:
-        restApi = requests.post(url = url, data = param)
-        print(restApi)
-        print("API is successfully fetched. HTTP Code: " + str(restApi.status_code))
-    except:
-        print("API failed to fetched. HTTP Code: " + str(restApi.status_code))
-    print(restApi.status_code)
+    elif args.operation == 'remove':
+        params['_do_confirm'] = 'yes'
+        params['_down_remove'] = 'Remove'
+
+    else:
+        print("Invalid operation. Use 'add' or 'remove'.")
+        return
+
+    response = send_request(url, params)
+    if response:
+        print(f"API response: {response}")
+        print(f"API request successful. HTTP Code: {response.status_code}")
+    else:
+        print("API request failed.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--operation", help="Provide Operation name add/remove", required=True)
-    parser.add_argument("-h", "--host", help="Operation to perform on hostname", required=True)
-    parser.add_argument("-t", "--time", help="Time duration to add host in downtime", default=None, required=False)
-    parser.add_argument("-c", "--comment", help="Comment to add host in downtime", default=None, required=False)
+    parser.add_argument("-o", "--operation", help="Provide Operation name add/remove", choices=['add', 'remove'], required=True)
+    parser.add_argument("-t", "--time", help="Time duration to add host in downtime", type=int, default=None)
+    parser.add_argument("-c", "--comment", help="Comment to add host in downtime", default=None)
     parser.add_argument("-u", "--username", help="Provide automation username", required=True)
     parser.add_argument("-p", "--password", help="Provide automation user password", required=True)
-    
+    parser.add_argument("-host", help="Operation to perform on hostname", required=True)  # Changed from -h to avoid conflict
+
     args = parser.parse_args()
-    
-    Main()
+    main()
